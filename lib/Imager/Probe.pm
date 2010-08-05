779d167294679a6c141a2b8ccc35e99a95c631d6
Tony Cook <tony@develop=help.com>
2010-08-05 09:53:31 +0000

    handle inccheck and libcheck probes

--- lib/Imager/Probe.pm
+++ lib/Imager/Probe.pm
@@ -6,33 +6,21 @@ use Config;
 sub probe {
   my ($class, $req) = @_;
 
+  my $name = $req->{name};
   my $result;
   if ($req->{code}) {
-    $result = _probe_code($req->{code});
+    $result = _probe_code($req);
   }
   if (!$result && $req->{pkg}) {
-    $result = _probe_pkg(@{$req->{pkg}});
+    $result = _probe_pkg($req);
   }
-  if (!$result) {
-    my $libcheck = $req->{libcheck};
-    if (!$libcheck && $req->{libname}) {
-      # synthesize a libcheck
-      my $lext=$Config{'so'};   # Get extensions of libraries
-      my $aext=$Config{'_a'};
-      my $libname = $req->{libname};
-      $libcheck = sub {
-	-e File::Spec->catfile($_[0], "$libname$aext")
-	  || -e File::Spec->catfile($_[0], "$libname.$lext")
-      };
-    }
-    if ($req->{inccheck} && $libcheck) {
-      $result = _probe_check($req->{inccheck}, $req->{libcheck});
-    }
+  if (!$result && $req->{inccheck} && ($req->{libcheck} || $req->{libbase})) {
+    $result = _probe_check($req);
   }
   $result or return;
 
   if ($req->{testcode}) {
-    $result = _probe_test($result, $req->{testcode});
+    $result = _probe_test($req, $result);
   }
 
   $result or return;
@@ -41,13 +29,14 @@ sub probe {
 }
 
 sub _probe_code {
-  my ($code) = @_;
+  my ($req) = @_;
 
+  my $code = $req->{code};
   my @probes = ref $code eq "ARRAY" ? @$code : $code;
 
   my $result;
   for my $probe (@probes) {
-    $result = $probe->()
+    $result = $probe->($req)
       and return $result;
   }
 
@@ -73,15 +62,15 @@ sub is_exe {
 }
 
 sub _probe_pkg {
-  my (@pkgs) = @_;
+  my ($req) = @_;
 
   $DB::single = 1;
   is_exe('pkg-config') or return;
   my $redir = $^O eq 'MSWin32' ? '' : '2>/dev/null';
 
+  my @pkgs = @{$req->{pkg}};
   for my $pkg (@pkgs) {
     if (!system("pkg-config $pkg --exists $redir")) {
-      print STDERR "Found $pkg\n";
       # if we find it, but the following fail, then pkg-config is too
       # broken to be useful
       my $cflags = `pkg-config $pkg --cflags`
@@ -92,16 +81,121 @@ sub _probe_pkg {
 
       chomp $cflags;
       chomp $lflags;
+      print "$req->{name}: Found via pkg-config $pkg\n";
       return
 	{
 	 INC => $cflags,
 	 LIBS => $lflags,
 	};
-
     }
   }
 
+  print "$req->{name}: Not found via pkg-config\n";
+
   return;
+}
+
+sub _probe_check {
+  my ($req) = @_;
+
+  my $libcheck = $req->{libcheck};
+  my $libbase = $req->{libbase};
+  if (!$libcheck && $req->{libbase}) {
+    # synthesize a libcheck
+    my $lext=$Config{'so'};   # Get extensions of libraries
+    my $aext=$Config{'_a'};
+    $libcheck = sub {
+      -e File::Spec->catfile($_[0], "lib$libbase$aext")
+	|| -e File::Spec->catfile($_[0], "lib$libbase.$lext")
+      };
+  }
+
+  my $found_libpath;
+  my @lib_search = _lib_paths($req);
+  for my $path (@lib_search) {
+    if ($libcheck->($path)) {
+      $found_libpath = $path;
+      last;
+    }
+  }
+
+  my $found_incpath;
+  my $inccheck = $req->{inccheck};
+  my @inc_search = _inc_paths($req);
+  for my $path (@inc_search) {
+    if ($inccheck->($path)) {
+      $found_incpath = $path;
+      last;
+    }
+  }
+
+  print "$req->{name}: includes ", $found_incpath ? "" : "not ",
+    "found - libraries ", $found_libpath ? "" : "not ", "found\n";
+
+  $found_libpath && $found_incpath
+    or return;
+
+  my @libs = "-L$found_libpath";
+  if ($req->{libopts}) {
+    push @libs, $req->{libopts};
+  }
+  elsif ($libbase) {
+    push @libs, "-l$libbase";
+  }
+  else {
+    die "$req->{name}: inccheck but no libbase or libopts";
+  }
+
+  return
+    {
+     INC => "-I$found_incpath",
+     LIBS => "@libs",
+    };
+}
+
+sub _lib_paths {
+  my ($req) = @_;
+
+  return _paths
+    (
+     $ENV{IM_LIBPATH},
+     $req->{libpath},
+     (
+      map { split ' ' }
+      grep $_,
+      @Config{qw/locincpath incpath libspath/}
+     ),
+     $^O eq "MSWin32" ? $ENV{LIB} : "",
+     $^O eq "cygwin" ? "/usr/lib/w32api" : "",
+    );
+}
+
+sub _inc_paths {
+  my ($req) = @_;
+
+  return _paths
+    (
+     $ENV{IM_INCPATH},
+     $req->{incpath},
+     $^O eq "MSWin32" ? $ENV{INCLUDE} : "",
+     $^O eq "cygwin" ? "/usr/include/w32api" : "",
+     "/usr/include",
+     "/usr/local/include",
+    );
+}
+
+sub _paths {
+  my (@in) = @_;
+
+  my @out;
+
+  for my $path (@in) {
+    $path or next;
+
+    push @out, grep -d $_, split /\Q$Config{path_sep}/, $path;
+  }
+
+  return @out;
 }
 
 1;
@@ -110,7 +204,7 @@ __END__
 
 =head1 NAME
 
-Imager::Probe - hot needle of enquiry for libraries
+Imager::Probe - hot needle of inquiry for libraries
 
 =head1 SYNOPSIS
 
@@ -127,8 +221,8 @@ Imager::Probe - hot needle of enquiry for libraries
 
 =head1 DESCRIPTION
 
-Does the probes that were hidden in Imager's Makefile.PL, pulled out
-so the file format libraries can be externalized.
+Does the probes that were hidden in Imager's F<Makefile.PL>, pulled
+out so the file format libraries can be externalized.
 
 The return value is either nothing if the probe fails, or a hash
 containing:
@@ -137,11 +231,11 @@ containing:
 
 =item *
 
-INC - -I and other C options
+C<INC> - C<-I> and other C options
 
 =item *
 
-LIBS - -L, -l and other link-time options
+C<LIBS> - C<-L>, C<-l> and other link-time options
 
 =back
 
@@ -151,29 +245,37 @@ The possible values for the hash supplied to the probe() method are:
 
 =item *
 
-cfg - an array of pkg-config names to probe for.  If the pkg-config
-checks pass, inccheck and libcheck aren't used.
+C<pkg> - an array of F<pkg-config> names to probe for.  If the
+F<pkg-config> checks pass, C<inccheck> and C<libcheck> aren't used.
 
 =item *
 
-inccheck - a code reference that checks if the supplied include
+C<inccheck> - a code reference that checks if the supplied include
 directory contains the required header files.
 
 =item *
 
-libcheck - a code reference that checks if the supplied library
-directory contains the required library files.  Note: the Makefile.PL
-version of this was supplied all of the library filenames instead.
+C<libcheck> - a code reference that checks if the supplied library
+directory contains the required library files.  Note: the
+F<Makefile.PL> version of this was supplied all of the library file
+names instead.
 
 =item *
 
-libfiles - if the libraries are found via inccheck/libcheck, these are
-the -l options to supply during the link phase.
+C<libbase> - if C<inccheck> is supplied, but C<libcheck> isn't, then a
+C<libcheck> that checks for C<lib>I<libbase>I<$Config{_a}> and
+C<lib>I<libbase>.I<$Config{so}> is created.  If C<libopts> isn't
+supplied then that can be synthesized as C<-l>C<<I<libbase>>>.
 
 =item *
 
-code - a code reference to perform custom checks.  Returns the probe
-result directly.  Can also be an array ref of functions to call.
+C<libopts> - if the libraries are found via C<inccheck>/C<libcheck>,
+these are the C<-l> options to supply during the link phase.
+
+=item *
+
+C<code> - a code reference to perform custom checks.  Returns the
+probe result directly.  Can also be an array ref of functions to call.
 
 =back
 
